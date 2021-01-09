a4d919a7cf967e62c695a5360ea7360f39d78442
Jesper Sørensen <jeppester1@gmail.com>
2021-01-09 22:18:25 +0100

    Use Cli#prompt for asana section prompt

--- providers/asana/tasks.rb
+++ providers/asana/tasks.rb
@@ -4,10 +4,11 @@ module Abt
   module Providers
     class Asana
       class Tasks
-        attr_reader :project_gid
+        attr_reader :project_gid, :cli
 
         def initialize(arg_str:, cli:)
           @project_gid = Asana.parse_arg_string(arg_str)[:project_gid]
+          @cli = cli
         end
 
         def call
@@ -31,7 +32,8 @@ module Abt
 
         def tasks
           @tasks ||= begin
-            asana.get_paged("tasks", section: section_gid)
+            section = cli.prompt 'Which section?', sections
+            asana.get_paged('tasks', section: section['gid'])
           end
         end
 
@@ -39,31 +41,6 @@ module Abt
           asana.get_paged("projects/#{project_gid}/sections")
         rescue Abt::HttpError::HttpError
           []
-        end
-
-        def section_gid
-          @section_gid ||= begin
-            section_gid = Abt::GitConfig.local('abt.asana.backlogSection')
-            if section_gid.nil?
-              section_gid = prompt_section['gid']
-              Abt::GitConfig.local('abt.asana.backlogSection', section_gid)
-            end
-            section_gid
-          end
-        end
-
-        def prompt_section
-          abort 'The project has no sections (columns)' if sections.empty?
-
-          sections.each_with_index do |section, index|
-            puts "#{section['name']} (#{index})"
-          end
-
-          puts "Please pick a backlog section (0-#{sections.length - 1})"
-          loop do
-            section = sections[STDIN.gets.strip.to_i]
-            return section unless section.nil?
-          end
         end
 
         def asana
