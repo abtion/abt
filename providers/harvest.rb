3b84853dd55f7a635116bc2aecd764b266080a84
Jesper Sørensen <jeppester1@gmail.com>
2021-01-09 15:42:39 +0100

    don't convert harvest ids to ints when it doesn't matter

--- providers/harvest.rb
+++ providers/harvest.rb
@@ -7,17 +7,17 @@ module Abt
         def parse_arg_string(arg_string)
           args = arg_string.to_s.split('/')
 
-          project_id = args[0].to_i
-          if project_id.zero?
-            project_id = Abt::GitConfig.local('abt.harvest.projectId').to_i
+          project_id = args[0].to_s
+          if project_id.empty?
+            project_id = Abt::GitConfig.local('abt.harvest.projectId').to_s
           end
-          project_id = nil if project_id.zero?
+          project_id = nil if project_id.empty?
 
-          task_id = args[1].to_i
-          if task_id.zero?
-            task_id = Abt::GitConfig.local('abt.harvest.taskId').to_i
+          task_id = args[1].to_s
+          if task_id.empty?
+            task_id = Abt::GitConfig.local('abt.harvest.taskId').to_s
           end
-          task_id = nil if project_id.zero?
+          task_id = nil if project_id.empty?
 
           { project_id: project_id, task_id: task_id }
         end
