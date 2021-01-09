08c92a8c988c7764e02df1233671fb3ca39a4074
Jesper Sørensen <jeppester1@gmail.com>
2021-01-09 22:22:59 +0100

    Add Asana::Move

---
+++ providers/asana/move.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module Abt
+  module Providers
+    class Asana
+      class Move
+        attr_reader :arg_str, :cli
+
+        def initialize(arg_str:, cli:)
+          @arg_str = arg_str
+          @cli = cli
+        end
+
+        def call
+          Current.new(arg_str: arg_str, cli: cli).call
+
+          section = cli.prompt 'Move asana task to?', sections
+
+          body = { data: { task: Abt::GitConfig.local('abt.asana.taskGid') } }
+
+          body_json = Oj.dump(body, mode: :json)
+          result = asana.post("sections/#{section['gid']}/addTask", body_json)
+          puts "Asana task moved to #{section['name']}"
+        rescue Abt::HttpError::HttpError => e
+          puts e
+          abort 'Unable to move asana task'
+        end
+
+        private
+
+        def project_gid
+          Abt::GitConfig.local('abt.asana.projectGid')
+        end
+
+        def sections
+          asana.get_paged("projects/#{project_gid}/sections")
+        rescue Abt::HttpError::HttpError
+          []
+        end
+
+        def asana
+          Abt::Asana::Client
+        end
+      end
+    end
+  end
+end
