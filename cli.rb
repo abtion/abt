d2b28d8397a059fafe121ab8a78588e6d988f8e8
Jesper Sørensen <jeppester1@gmail.com>
2021-01-13 11:48:10 +0100

    Add README.md and command to print it

--- cli.rb
+++ cli.rb
@@ -19,6 +19,8 @@ module Abt
         print_help
       elsif ['--help', '-h', 'help', 'commands'].include?(command)
         print_help
+      elsif command === 'help-md'
+        print_help_md
       end
       abort('No provider arguments') if args.empty?
 
@@ -81,6 +83,11 @@ module Abt
       exit
     end
 
+    def print_help_md
+      puts help_md
+      exit
+    end
+
     def help_text
       text = StringIO.new
 
@@ -117,6 +124,54 @@ module Abt
         max_length = command_texts.map(&:first).map(&:length).max
         command_texts.each do |(command, description)|
           text.puts("  #{command.ljust(max_length)} # #{description}")
+        end
+      end
+
+      text.string.strip!
+    end
+
+    def help_md
+      text = StringIO.new
+
+      text.puts <<~MD
+        # Abt
+        This readme was generated with `abt help-md > README.md`
+
+        ## Usage
+        `abt <command> [<provider:arguments>...]`
+
+        Multiple providers and arguments can be passed, e.g.:
+        - `abt init asana harvest`
+        - `abt pick-task asana harvest`
+        - `abt start asana harvest`
+        - `abt clear asana harvest`
+
+        Command output can be piped, e.g.:
+        - `abt tasks asana | grep -i <name of task>`
+        - `abt tasks asana | grep -i <name of task> | abt start`
+
+        ## Available commands:
+      MD
+
+      Abt::Providers.constants.sort.each_with_index do |provider_name, index|
+        text.puts '' unless index.zero?
+        text.puts "### #{provider_name}"
+        text.puts '| Command | Description |'
+        text.puts '| :------ | :---------- |'
+
+        provider_class = Abt::Providers.const_get(provider_name)
+        command_texts = []
+        provider_class.constants.sort.each do |command_name|
+          command_class = provider_class.const_get(command_name)
+
+          if command_class.respond_to? :command
+            command_texts.push([command_class.command, command_class.description])
+          end
+        end
+
+        max_length = command_texts.map(&:first).map(&:length).max
+        command_texts.each do |(command, description)|
+          text.puts("| `#{command.ljust(max_length)}` | #{description} |")
         end
       end
 
