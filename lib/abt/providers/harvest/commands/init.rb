5868d0e1034ff811533fd9712616005acd9de105
Jesper Sørensen <jeppester1@gmail.com>
2021-01-19 13:18:29 +0100

    Harvest init: Make client name searchable

--- lib/abt/providers/harvest/commands/init.rb
+++ lib/abt/providers/harvest/commands/init.rb
@@ -39,20 +39,29 @@ module Abt
 
               cli.warn 'Showing the 10 first matches' if matches.size > 10
               choice = cli.prompt_choice 'Select a project', matches[0...10], true
-              break choice unless choice.nil?
+              break choice['project'] unless choice.nil?
             end
           end
 
           def matches_for_string(string)
             search_string = sanitize_string(string)
 
-            projects.select do |project|
+            searchable_projects.select do |project|
               sanitize_string(project['name']).include?(search_string)
             end
           end
 
           def sanitize_string(string)
             string.downcase.gsub(/[^\w]/, '')
+          end
+
+          def searchable_projects
+            @searchable_projects ||= projects.map do |project|
+              {
+                'name' => "#{project['client']['name']} > #{project['name']}",
+                'project' => project
+              }
+            end
           end
 
           def projects
