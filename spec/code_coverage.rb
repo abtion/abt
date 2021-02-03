2fe5b662a43ec7945a81356552080deda7bec813
Jesper Sørensen <jeppester1@gmail.com>
2021-02-03 22:35:00 +0100

    SimpleCov: Exclude lib/abt/docs and lib/abt/cli from Core group
    They are covered by Docs and Cli group respectively

--- spec/code_coverage.rb
+++ spec/code_coverage.rb
@@ -6,7 +6,7 @@ unless ENV['DISABLE_SIMPLECOV'] == 'true'
   SimpleCov.start do
     add_filter 'spec'
 
-    add_group 'Core', ['lib/abt.rb', %r{lib/abt/[^/]+.rb}]
+    add_group 'Core', ['lib/abt.rb', %r{lib/abt/(?!docs|cli)[^/]*.rb}]
     add_group 'Cli', ['lib/abt/cli']
     add_group 'Docs', ['lib/abt/docs']
     add_group 'DevOps', 'lib/abt/providers/devops'
