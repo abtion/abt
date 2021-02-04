6008e4e3cc63e15a02b9630604de0f34e8139905
Jesper Sørensen <jeppester1@gmail.com>
2021-02-04 10:40:26 +0100

    Add Asana BranchName

---
+++ lib/abt/providers/asana/commands/branch-name.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Abt
+  module Providers
+    module Asana
+      module Commands
+        class BranchName < BaseCommand
+          def self.command
+            'branch-name asana[:<project-gid>/<task-gid>]'
+          end
+
+          def self.description
+            'Suggest a git branch name for the current/specified task.'
+          end
+
+          def call
+            require_task!
+            ensure_current_is_valid!
+
+            cli.puts name
+          end
+
+          private
+
+          def name
+            task['name'].downcase.gsub(/[^\w]+/, '-')
+          end
+
+          def ensure_current_is_valid!
+            cli.abort "Invalid task gid: #{task_gid}" if task.nil?
+
+            return if task['memberships'].any? { |m| m.dig('project', 'gid') == project_gid }
+
+            cli.abort "Invalid project gid: #{project_gid}"
+          end
+
+          def task
+            @task ||= api.get("tasks/#{task_gid}", opt_fields: 'name,memberships.project')
+          end
+        end
+      end
+    end
+  end
+end
