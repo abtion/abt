67d9079357d8c934c51b0b2bfaf12759f3a3a7a9
Jesper Sørensen <jeppester1@gmail.com>
2021-02-04 10:29:57 +0100

    Add DevOps BranchName

---
+++ lib/abt/providers/devops/commands/branch-name.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Abt
+  module Providers
+    module Devops
+      module Commands
+        class BranchName < BaseCommand
+          def self.command
+            'branch-name devops[:<organization-name>/<project-name>/<board-id>/<work-item-id>]'
+          end
+
+          def self.description
+            'Suggest a git branch name for the current/specified work-item.'
+          end
+
+          def call
+            require_work_item!
+
+            cli.puts name
+          rescue HttpError::NotFoundError
+            args = [organization_name, project_name, board_id, work_item_id].compact
+            cli.warn 'Unable to find work item for configuration:'
+            cli.abort "devops:#{args.join('/')}"
+          end
+
+          private
+
+          def name
+            str = work_item['id']
+            str += '-'
+            str += work_item['name'].downcase.gsub(/[^\w]/, '-')
+            str.gsub(/-+/, '-')
+          end
+
+          def work_item
+            @work_item ||= begin
+              work_item = api.get_paged('wit/workitems', ids: work_item_id)[0]
+              sanitize_work_item(work_item)
+            end
+          end
+        end
+      end
+    end
+  end
+end
