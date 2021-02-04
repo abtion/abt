fe871de0706e0487e034351d27217e1b88b9b6ef
Jesper Sørensen <jeppester1@gmail.com>
2021-02-04 11:31:34 +0100

    Add Git Branch

---
+++ lib/abt/providers/git.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Dir.glob("#{File.expand_path(__dir__)}/git/*.rb").sort.each { |file| require file }
+Dir.glob("#{File.expand_path(__dir__)}/git/commands/*.rb").sort.each { |file| require file }
+
+module Abt
+  module Providers
+    module Git
+      def self.command_names
+        Commands.constants.sort.map { |constant_name| Helpers.const_to_command(constant_name) }
+      end
+
+      def self.command_class(name)
+        const_name = Helpers.command_to_const(name)
+        Commands.const_get(const_name) if Commands.const_defined?(const_name)
+      end
+    end
+  end
+end
---
+++ lib/abt/providers/git/commands/branch.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+module Abt
+  module Providers
+    module Git
+      module Commands
+        class Branch
+          attr_reader :cli
+
+          def self.command
+            'branch git <provider>'
+          end
+
+          def self.description
+            'Switch branch. Uses a compatible provider to generate the branch-name: E.g. `abt branch git asana`'
+          end
+
+          def initialize(cli:, **)
+            @cli = cli
+          end
+
+          def call
+            create_and_switch unless switch
+            cli.warn "Switched to #{branch_name}"
+          end
+
+          private
+
+          def switch
+            success = false
+            Open3.popen3("git switch #{branch_name}") do |_i, _o, _error_output, thread|
+              success = thread.value.success?
+            end
+            success
+          end
+
+          def create_and_switch
+            cli.warn "No such branch: #{branch_name}"
+            cli.abort('Aborting') unless cli.prompt_boolean 'Create branch?'
+
+            Open3.popen3("git switch -c #{branch_name}") do |_i, _o, _e, thread|
+              thread.value
+            end
+          end
+
+          def branch_name # rubocop:disable Metrics/MethodLength
+            @branch_name ||= begin
+              if branch_names_from_providers.empty?
+                cli.abort [
+                  'None of the specified providers responded to `branch-name`.',
+                  'Did you add compatible provider? e.g.:',
+                  '   abt branch git asana',
+                  '   abt branch git devops'
+                ].join("\n")
+              end
+
+              if branch_names_from_providers.length > 1
+                cli.abort [
+                  'Got branch names from multiple providers, only one is supported',
+                  'Branch names where:',
+                  *branch_names_from_providers.map { |name| "   #{name}" }
+                ].join("\n")
+              end
+
+              branch_names_from_providers.first
+            end
+          end
+
+          def branch_names_from_providers
+            input = StringIO.new(cli.args.join(' '))
+            output = StringIO.new
+            Abt::Cli.new(argv: ['branch-name'], output: output, input: input).perform
+
+            output.string.lines.map(&:strip).compact
+          end
+        end
+      end
+    end
+  end
+end
