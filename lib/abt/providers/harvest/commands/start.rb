ea9aee8ac4b6445e29eaf2687f2491572c5af36e
Jesper Sørensen <jeppester1@gmail.com>
2021-02-12 00:40:35 +0100

    Fix typo

--- lib/abt/providers/harvest/commands/start.rb
+++ lib/abt/providers/harvest/commands/start.rb
@@ -12,7 +12,7 @@ module Abt
           end
 
           def self.description
-            'Alias for: `abt track harvest`. Meant to used in combination other scheme arguments, e.g. `abt start harvest asana`'
+            'Alias for: `abt track harvest`. Meant to used in combination with other scheme arguments, e.g. `abt start harvest asana`'
           end
         end
       end
