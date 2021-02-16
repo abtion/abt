4438b65911e34d770b006c71d917e098345b133d
Jesper Sørensen <jeppester1@gmail.com>
2021-02-16 09:35:02 +0100

    Add specs for Harvest Start

---
+++ spec/abt/providers/harvest/commands/start_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe(Abt::Providers::Harvest::Commands::Start, :harvest) do
+  it 'is a subclass of Track' do
+    expect(Abt::Providers::Harvest::Commands::Start.superclass).to(
+      be(Abt::Providers::Harvest::Commands::Track)
+    )
+  end
+
+  it 'has it\'s own description and usage' do
+    expect(Abt::Providers::Harvest::Commands::Start.usage).to_not(
+      eq(Abt::Providers::Harvest::Commands::Track.usage)
+    )
+
+    expect(Abt::Providers::Harvest::Commands::Start.description).to_not(
+      eq(Abt::Providers::Harvest::Commands::Track.description)
+    )
+  end
+end
