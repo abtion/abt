076b32c68038e07072c7600be9a442d54cdc0cd6
Jesper Sørensen <jeppester1@gmail.com>
2021-03-10 22:17:54 +0100

    Remove commented out method

--- spec/support/devops_helpers.rb
+++ spec/support/devops_helpers.rb
@@ -15,23 +15,4 @@ module DevopsHelpers
 
     stub_request(verb, path, *rest).with(headers: headers, basic_auth: basic_auth)
   end
-
-  # def stub_get_projects(git_config, query, projects)
-  #   # Create a page per record to force the pagination code into action
-  #   path = 'projects'
-  #   used_query = query.merge(workspace: git_config['workspaceGid'], limit: 100)
-
-  #   projects.each_with_index do |project, index|
-  #     response_data = { "data": [project] }
-
-  #     next_path = "projects?offset=#{index}"
-  #     response_data['next_page'] = { 'path' => "/#{next_path}" } if index != projects.length - 1
-
-  #     stub_asana_request(git_config, :get, path)
-  #       .with(query: used_query)
-  #       .to_return(body: Oj.dump(response_data, mode: :json))
-
-  #     path = next_path
-  #   end
-  # end
 end
